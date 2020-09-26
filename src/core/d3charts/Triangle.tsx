1bc90b0784ea6011dc6428f90cf4ee5187fd474b
Orkhan Huseynli <o.huseynli@gmail.com>
2020-09-26 11:54:38 +0200

    drawing triangle

--- src/core/d3charts/Triangle.tsx
+++ src/core/d3charts/Triangle.tsx
@@ -64,7 +64,11 @@ let buildSvg = (targetId: string) => {
 
     let xScale = d3.scaleLinear().domain([0, 100]).range([0, 800]);
     let yScale = d3.scaleLinear().domain([0, 100]).range([800, 0]);
-    let data = [[0, 50], [100, 80], [200, 40], [300, 60], [400, 30]];
+   // let data = [[0, 50], [100, 80], [200, 40], [300, 60], [400, 30]];
+   let e1 = new Edge([30,38], [60,38]);
+   let e2 = new Edge([60,38], [45,63]);
+   let e3 = new Edge([45,63], [30,38]);
+    let data = buildTriangle(e1, e2, e3); 
     let lineGenerator = d3.line()
                         .x((data)=> {return xScale(data[0]);})
                         .y((data) => {return yScale(data[1]);});
@@ -72,3 +76,34 @@ let buildSvg = (targetId: string) => {
     svg.append('path').attr('d', lineGenerator(data));
     return svg.node();
 };
+
+class Edge {
+    public p1:number[];
+    public p2:number[];
+    constructor(p1: number[], p2:number[]){
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+}
+
+let buildTriangle = (e1:Edge, e2:Edge, e3:Edge):number[][]=>{
+    let collectedPoints: number[][] = [];
+    if (!compareArrays(e1.p2,e2.p1) || !compareArrays(e2.p2,e3.p1) || !compareArrays(e3.p2,e1.p1)) {
+        throw Error("Edges are not in the correct order");
+    }
+    collectedPoints.push(e1.p1);
+
+    collectedPoints.push(e1.p2);
+    collectedPoints.push(e2.p1);
+
+    collectedPoints.push(e2.p2);
+    collectedPoints.push(e3.p1);
+
+    collectedPoints.push(e3.p2);
+    collectedPoints.push(e1.p1);
+    return collectedPoints;
+}
+
+let compareArrays = (ar1:number[], ar2:number[]): boolean => {
+    return JSON.stringify(ar1) == JSON.stringify(ar2);
+}
